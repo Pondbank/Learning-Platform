fe377d9bda15a4145d453b2c0fbc91b75f18312a
Pondbank <markthevoyager@gmail.com>
2026-07-19 22:56:42 +0100

    Update API_URL in config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbwYvzOUfPykebXaTL9hUrMjelcJk9lTGr-Tt5vudaUmk4OC1wJSOm-4P7pkTHRNnT1o/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycby8Pjor1Y5Kh97ZpBRsMYs8RD-_E1U5-Uovz666uBq10WJl78hvRJV62ujSPkRea22ZLg/exec",
   // These can be left blank — the app will fetch them from the backend via getConfig().
   // But hardcoding them avoids a loading flash.
   GOOGLE_CLIENT_ID: "",
